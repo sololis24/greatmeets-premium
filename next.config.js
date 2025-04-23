015a4f84a42327d2405fe0f44b34669f664a11aa
Lis <lis@Liss-MacBook-Air.local>
2025-04-23 10:07:33 +0200

    🛠 Fix: Add Webpack aliases for Netlify

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
-const path = require("path");
-
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@components': path.resolve(__dirname, 'components'),
+      '@firebase': path.resolve(__dirname, 'firebase'),
+      '@lib': path.resolve(__dirname, 'lib'),
+    };
     return config;
   },
 };
 
+const path = require('path');
 module.exports = nextConfig;
